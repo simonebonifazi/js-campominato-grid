559a88a0377e23113396cf317c4a9a4a723aa750
simonebonifazi <simonbonifraz@gmail.com>
2022-07-01 16:18:24 +0200

    partially-refactured

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 /*
 Traccia:
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Procediamo milestone per milestone senza invertire l'ordine.
-Cerchiamo di fare almeno un commit per ogni milestone!
 # BONUS
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
@@ -43,17 +37,31 @@ dove poi stamperò in console il suo numero e tramite css la coloro di azzurro.
 0. eventlistener sulla singola cella MA la vedo solo nel ciclo for!
 */
 
+/* *********************************************
+creo la funzione per creare celle */
+function createCell() {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+
+    return cell;
+}
+
+
 
 // milestone 2. 0.
 const gameStarter = document.getElementById('game-starter');
 const gameArea = document.getElementById('perimeter');
 
-//1. in questo caso mi chiedo: mi può tornare utile il .this? per stabilire se adottare arrow f o f ; non lo so, non ho ben capito .this
+//1. 
+//2.bis rinomino e creo delle costanti in funzione della traccia del bonus:
+let rows = 10;
+//poichè la mia griglia sarà sempre quadrata
+let cols = rows;
+let totalRowsCols = rows * cols;
 gameStarter.addEventListener('click', function () {
     //2.
-    for (let i = 1; i <= 100; i++) {
-        const cellElement = document.createElement('div');
-        cellElement.className = 'cell';
+    for (let i = 1; i <= totalRowsCols; i++) {
+        const cellElement = createCell();
         //milestone 3. 
         cellElement.innerText = i;
         gameArea.appendChild(cellElement);
@@ -61,11 +69,12 @@ gameStarter.addEventListener('click', function () {
         //milestone 4.
         cellElement.addEventListener('click', function () {
             console.log(i);
-            //in fase di refactoring adotterò il toggle che mi piace di più
-            cellElement.classList.add('clicked');
+
+            cellElement.classList.toggle('clicked');
         })
 
     }
 })
+
     // #MILESTONE 4
     // Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
