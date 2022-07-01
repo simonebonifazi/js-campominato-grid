35b8c39540bce8e7c93932345693b1d777525328
simonebonifazi <simonbonifraz@gmail.com>
2022-07-01 15:55:08 +0200

    added-milestone-2-and-css

--- js/script.js
+++ js/script.js
@@ -6,14 +6,6 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 Procediamo milestone per milestone senza invertire l'ordine.
 Cerchiamo di fare almeno un commit per ogni milestone!
-# MILESTONE 1
-Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-#MILESTONE 2
-Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-#MILESTONE 3
-In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
-#MILESTONE 4
-Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
 # BONUS
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
@@ -27,4 +19,34 @@ Ad esempio:
 Di cosa ho bisogno per generare i numeri?
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento
+
+#MILESTONE 2
+Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS.
+Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+#MILESTONE 3
+In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
+#MILESTONE 4
+Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
+
 */
+
+/*
+steps milestone 2-- generazione celle
+0. creazione variabili globali connesse al DOM
+1. aggiungo evento allo scatenarsi del quale si generano le mie celle tramite |<=
+2.|=> ciclo for per creare elementi da inserire nel testo (con metodo appendChild)
+3. ? può servirmi una funzione? ? 
+*/
+//0.
+const gameStarter = document.getElementById('game-starter');
+const gameArea = document.getElementById('perimeter');
+
+//1. in questo caso mi chiedo: mi può tornare utile il .this? per stabilire se adottare arrow f o f ; non lo so, non ho ben capito .this
+gameStarter.addEventListener('click', function () {
+    for (let i = 1; i <= 100; i++) {
+        const cellElement = document.createElement('div');
+        cellElement.className = 'cell';
+
+        gameArea.appendChild(cellElement);
+    }
+})
